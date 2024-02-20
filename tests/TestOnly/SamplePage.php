80e7b7c30f4d712007ead8b643d78c93e0ffa22c
Nic Horstmeier <nhorstmeier@dynamicagency.com>
2024-02-20 14:25:42 -0600

    UPDATE sample page one

--- tests/TestOnly/SamplePage.php
+++ tests/TestOnly/SamplePage.php
@@ -8,13 +8,6 @@ use SilverStripe\Dev\TestOnly;
 class SamplePage extends \Page implements TestOnly
 {
     /**
-     * @var string[]
-     */
-    private static array $disallowed_elements = [
-        ElementOne::class,
-    ];
-
-    /**
      * @var array|string[]
      */
     private static array $extensions = [
