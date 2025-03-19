38f6d573a1f70dd9956a122ba02ddc7ad7475d23
Jason Irish <jirish@dynamicagency.com>
2025-03-18 20:24:37 -0500

    REFACTOR CMSPageAddControllerExtension updateDoAdd()
    Previously only worked if the ElementalArea relation was named `ElementalArea`.
    
    This will find the first ElementalArea for the class that has a field in getCMSFields(). For example, on `Dynamic\Base\Page\HomePage`, we add has_one `ElementalHomePage` and hide the `ElementalArea` field. In this case, blocks would be copied to `ElementalHomePage`

---
+++ tests/TestOnly/TestTemplate.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Dynamic\ElememtalTemplates\Tests\TestOnly;
+
+use Dynamic\ElememtalTemplates\Models\Template;
+
+class TestTemplate extends Template
+{
+    private static $table_name = 'TestTemplate';
+}
