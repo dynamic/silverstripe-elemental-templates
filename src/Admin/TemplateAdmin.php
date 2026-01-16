c01a74cdd2d1ff447ddd3be9dcb266b8824a0e9e
Jason Irish <jirish@dynamicagency.com>
2026-01-16 13:37:49 -0600

    fix: Remove broken GridField preview action
    - Removed GridFieldPreviewAction component that wasn't functioning
    - Reverted TemplateAdmin to simple ModelAdmin
    - Created Issue #51 to implement working preview in admin later

--- src/Admin/TemplateAdmin.php
+++ src/Admin/TemplateAdmin.php
@@ -4,8 +4,6 @@ namespace Dynamic\ElementalTemplates\Admin;
 
 use Dynamic\ElementalTemplates\Models\Template;
 use SilverStripe\Admin\ModelAdmin;
-use SilverStripe\Forms\Form;
-use SilverStripe\Forms\GridField\GridField;
 
 /**
  * Class \Dynamic\ElementalTemplates\Admin\TemplateAdmin
@@ -34,30 +32,4 @@ class TemplateAdmin extends ModelAdmin
      * @var string
      */
     private static string $menu_icon_class = 'font-icon-block-layout';
-
-    /**
-     * Customize the GridField to add a Preview action
-     *
-     * @param int|null $id
-     * @param \SilverStripe\Forms\FieldList|null $fields
-     * @return Form
-     */
-    public function getEditForm($id = null, $fields = null): Form
-    {
-        $form = parent::getEditForm($id, $fields);
-
-        if ($this->modelClass === Template::class) {
-            /** @var GridField $gridField */
-            $gridField = $form->Fields()->dataFieldByName($this->sanitiseClassName($this->modelClass));
-
-            if ($gridField instanceof GridField) {
-                $config = $gridField->getConfig();
-
-                // Add preview action component
-                $config->addComponent(new GridFieldPreviewAction());
-            }
-        }
-
-        return $form;
-    }
 }
